4993bb70342a05c564accffe80d910e16487bfbb
Blassel228 <hok.ykrop@gmail.com>
2025-01-24 15:10:24 +0200

    (components) button was added to the Button.tsx, however needs to be changed

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,5 +1,30 @@
 import styled from "styled-components";
 
+
+export const UploadActionButton = styled.button`
+  padding: 1rem 2rem;
+  background-color: #28a745;
+  color: #eae3e3;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1.2rem;
+  font-weight: bold;
+  transition: all 0.3s ease;
+  margin-top: 1rem;
+
+  &:hover {
+    background-color: #1e7e34;
+  }
+
+  &:disabled {
+    background-color: #565050;
+    color: #beb4b4;
+    cursor: not-allowed;
+  }
+`;
+
+
 const Button = styled.button`
   padding: 1rem 2rem;
   font-size: 1.6rem;
